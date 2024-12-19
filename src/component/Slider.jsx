ac71434b36375bdd54002f188bca8c98a2b7ce41
Satyen0592 <satyen212@gmail.com>
2024-12-19 22:14:33 +0530

    Slider

---
+++ src/component/Slider.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import GlobalApi from "../Services/GlobalApi";
+
+function Slider() {
+  useEffect(() => {
+    getTrendingMOvies();
+  }, []);
+  const getTrendingMOvies = () => {
+    GlobalApi.getTrendingVideos.then((resp) => {
+      console.log(resp);
+    });
+  };
+  return <div>Slider</div>;
+}
+
+export default Slider;
