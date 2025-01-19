1cdf6b63ba307cbe34578ceb8ae4d55bd21fe5dd
Satyen0592 <satyen212@gmail.com>
2025-01-19 21:37:45 +0530

    ....

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,16 +1,16 @@
-import axios from "axios";
+// import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: "http://disneyclone.com",
-  params: {
-    api_key: "a3da97f9a263f165b4788453da2eae7a",
-  },
-});
+// const axiosInstance = axios.create({
+//   baseURL: "http://disneyclone.com",
+//   params: {
+//     api_key: "a3da97f9a263f165b4788453da2eae7a",
+//   },
+// });
 
-const getTrendingVideos = () => axiosInstance.get("/trending/all/day");
+// const getTrendingVideos = () => axiosInstance.get("/trending/all/day");
 
-const getMovieByGenreId = (id) =>
-  axiosInstance.get("/discover/movie", { params: { with_genres: id } });
+// const getMovieByGenreId = (id) =>
+//   axiosInstance.get("/discover/movie", { params: { with_genres: id } });
 
 // export default {
 //   getTrendingVideos,
