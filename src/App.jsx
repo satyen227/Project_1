7c479da3a2270f958fef1bd7a42bb9f9fda5744b
Satyen0592 <satyen212@gmail.com>
2025-02-16 23:33:38 +0530

    .....

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./component/Header";
 import Slider from "./component/slider";
+import HrMoviesCard from "./component/HrMoviesCard";
 
 function App() {
   return (
     <div>
       <Header />
       <Slider />
+      <HrMoviesCard />
     </div>
   );
 }
--- src/component/HrMoviesCard.jsx
+++ src/component/HrMoviesCard.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import "react";
 
 function HrMoviesCard() {
-  return <div>HrMoviesCard</div>;
+  return (
+    <div
+      style={{
+        width: 100,
+        height: 100,
+        backgroundColor: "whitesmoke",
+        margin: 10,
+        hover: "scale-110",
+      }}
+    >
+      HrMoviesCard
+    </div>
+  );
 }
 
-// export default HrMoviesCard;
+export default HrMoviesCard;
