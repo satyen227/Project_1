662ae90c7073e61ba23917ba2a5c13c972d83bc0
Satyen0592 <satyen212@gmail.com>
2025-02-18 21:40:03 +0530

    Slider UI changes

--- src/component/Slider.jsx
+++ src/component/Slider.jsx
@@ -1,79 +1,58 @@
 import { useEffect, useRef, useState } from "react";
 import GlobalApi from "../Services/GlobalApi";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
-
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
-
+const screenWidth = window.innerWidth;
 function Slider() {
   const [movieList, setMovieList] = useState([]);
-  const sliderRef = useRef(null);
-
+  const elementRef = useRef();
   useEffect(() => {
     getTrendingMovies();
   }, []);
 
-  const getTrendingMovies = async () => {
-    try {
-      const resp = await GlobalApi.getTrendingVideos();
+  const getTrendingMovies = () => {
+    GlobalApi.getTrendingVideos().then((resp) => {
+      console.log(resp.data.results);
       setMovieList(resp.data.results);
-    } catch (error) {
-      console.error("Error fetching trending movies:", error);
-    }
+    });
   };
 
-  const scrollSlider = (direction) => {
-    if (sliderRef.current) {
-      const slideWidth = sliderRef.current.clientWidth / 4; // Each slide takes 1/4th of the screen
-      const maxScroll =
-        sliderRef.current.scrollWidth - sliderRef.current.clientWidth;
-
-      let newScrollLeft =
-        direction === "right"
-          ? sliderRef.current.scrollLeft + slideWidth
-          : sliderRef.current.scrollLeft - slideWidth;
-
-      // Infinite loop effect
-      if (newScrollLeft >= maxScroll) {
-        newScrollLeft = 0; // Reset to beginning
-      } else if (newScrollLeft <= 0) {
-        newScrollLeft = maxScroll; // Jump to end
-      }
-
-      sliderRef.current.scrollTo({
-        left: newScrollLeft,
-        behavior: "smooth",
-      });
-    }
+  const sliderRight = (element) => {
+    element.scrollLeft += screenWidth - 110;
   };
-
+  const sliderLeft = (element) => {
+    element.scrollLeft -= screenWidth - 110;
+  };
   return (
-    <div className="relative w-full overflow-hidden">
-      {/* Left Arrow */}
+    <div className="relative">
       <HiChevronLeft
-        className="absolute left-4 top-1/2 transform -translate-y-1/2 z-10 bg-black bg-opacity-50 p-3 rounded-full text-white text-4xl cursor-pointer hover:scale-110 transition"
-        onClick={() => scrollSlider("left")}
+        className="hidden md:block text-white text-[30px] absolute
+        mx-8 top-1/2 transform -translate-y-1/2 cursor-pointer "
+        onClick={() => sliderLeft(elementRef.current)}
       />
-
-      {/* Right Arrow */}
       <HiChevronRight
-        className="absolute right-4 top-1/2 transform -translate-y-1/2 z-10 bg-black bg-opacity-50 p-3 rounded-full text-white text-4xl cursor-pointer hover:scale-110 transition"
-        onClick={() => scrollSlider("right")}
+        className="hidden md:block text-white text-[30px] absolute
+        mx-8 top-1/2 transform -translate-y-1/2 cursor-pointer right-0"
+        onClick={() => sliderRight(elementRef.current)}
       />
 
-      {/* Slider Container */}
       <div
-        ref={sliderRef}
-        className="flex w-full overflow-x-auto scroll-smooth scrollbar-none space-x-4 px-10"
+        className="flex overflow-x-auto w-full px-16 py-4 gap-4
+    scrollbar-none scroll-smooth"
+        ref={elementRef}
+        style={{ height: "410px" }}
       >
-        {movieList.map((movie, index) => (
+        {movieList.map((item) => (
           <div
-            key={movie.id}
-            className="flex-none w-1/4 h-[200px] md:h-[250px] lg:h-[300px] transition-transform duration-500 ease-in-out transform hover:scale-105 hover:z-10"
+            key={item.id}
+            className="flex-shrink-0"
+            style={{ height: "100%" }}
           >
             <img
-              src={IMAGE_BASE_URL + movie.backdrop_path}
-              alt={movie.title || "Movie Image"}
-              className="w-full h-full object-cover rounded-lg shadow-lg"
+              src={IMAGE_BASE_URL + item.backdrop_path}
+              className="w-full h-full object-cover
+              object-left-top mr-5 rounded-md hover:border-[4px]
+              border-gray-400 transition-all duration-100 ease-in"
             />
           </div>
         ))}
